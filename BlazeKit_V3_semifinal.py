7e5da4447deeeb0c52f9ae0da7a94aeee7e6b53c
Rishabh Jain <rishabhking05@gmail.com>
2023-02-23 15:44:07 -0600

    scrollbar added

--- BlazeKit_V3_semifinal.py
+++ BlazeKit_V3_semifinal.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import scrolledtext, Text
 import subprocess
 import tkinter.simpledialog as simpledialog
 
@@ -8,19 +9,26 @@ root.geometry("1620x1080")
 root.title("BlazeKit")
 
 # Create a frame for the left column
-left_frame = tk.Frame(root)
+left_frame = tk.Frame(root) #add bg tag after comma to ad background to button panel. (root, bg = 'red')
 left_frame.pack(side=tk.LEFT, fill=tk.Y)
 
 def clear_screen():
     text_widget.delete('1.0', tk.END)
-    result = subprocess.run(['clear'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = result.stdout.decode() + result.stderr.decode()
-    text_widget.insert(tk.END, output)
+    subprocess.run(['clear'])
 
 def brew_kubectl():
     result = subprocess.run(['brew', 'install', 'kubectl'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = result.stdout.decode() + result.stderr.decode()
     text_widget.insert(tk.END, "\nOutput of 'brew install kubectl':\n")
+    text_widget.insert(tk.END, output)
+    text_widget.insert(tk.END, "\nDone\n")
+
+def download_aws_cli():
+    result = subprocess.run(['curl', 'https://awscli.amazonaws.com/AWSCLIV2.pkg', '-o', 'AWSCLIV2.pkg'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(['curl', 'sudo', 'installer', '-pkg', 'AWSCLIV2.pkg', '-target', '/'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = result.stdout.decode() + result.stderr.decode()
+    text_widget.insert(tk.END, "\nOutput of 'curl https://awscli.amazonaws.com/AWSCLIV2.pkg -o AWSCLIV2.pkg':\n")
+    text_widget.insert(tk.END, "\nOutput of 'curl sudo installer -pkg AWSCLIV2.pkg -target /':\n")
     text_widget.insert(tk.END, output)
     text_widget.insert(tk.END, "\nDone\n")
 
@@ -83,10 +91,13 @@ def show_deployment_details():
 
 # Button Configuration Secction
 
-button = tk.Button(left_frame, text=f"Clear Screen", command=clear_screen)
+button = tk.Button(left_frame, text=f"Clear Screen", command=clear_screen, fg="red")
 button.pack(side=tk.TOP, pady=5)
 
 button = tk.Button(left_frame, text=f"Install Kubectl", command=brew_kubectl)
+button.pack(side=tk.TOP, pady=5)
+
+button = tk.Button(left_frame, text=f"Install AWS_CLI", command=download_aws_cli)
 button.pack(side=tk.TOP, pady=5)
 
 button = tk.Button(left_frame, text=f"Cluster Health", command=cluster_health)
@@ -107,16 +118,23 @@ button.pack(side=tk.TOP, pady=5)
 button = tk.Button(left_frame, text=f"Show Deployments", command=show_deployments)
 button.pack(side=tk.TOP, pady=5)
 
-button = tk.Button(left_frame, text=f"Show Deployments Details", command=show_deployment_details)
+button = tk.Button(left_frame, text=f"Show Deployment Details", command=show_deployment_details)
 button.pack(side=tk.TOP, pady=5)
 
 # Create a frame for the right column
 right_frame = tk.Frame(root, bd=2, relief=tk.SOLID, highlightthickness=1, highlightbackground="black")
 right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
+# Create a scrollbar
+scrollbar = tk.Scrollbar(right_frame)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
 # Create a text input in the right frame
-text_widget = tk.Text(right_frame)
+text_widget = Text(right_frame, yscrollcommand=scrollbar.set, wrap=tk.WORD)
 text_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+# Configure the scrollbar to link to the text widget
+scrollbar.config(command=text_widget.yview )
 
 # Start the main event loop
 root.mainloop()
